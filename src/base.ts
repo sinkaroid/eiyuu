114bccb7df008a3aba6636558b048acf1cbd5788
sinkaroid <bibsindra@gmail.com>
2022-08-15 06:26:04 +0700

    feat: add constant

---
+++ src/base.ts
@@ -0,0 +1,13 @@
+export default {
+  GELBOORU: "https://gelbooru.com/index.php?page=tags&s=list&tags=",
+  DANBOORU: "https://danbooru.donmai.us/tags?commit=Search&search%5Bhide_empty%5D=yes&search%5Bname_or_alias_matches%5D=",
+  HYPNOHUB: "https://hypnohub.net/index.php?page=tags&s=list&tags=",
+  KONACHAN: "https://konachan.com/tag?name=",
+  LOLIBOORU: "https://lolibooru.moe/tag?name=",
+  RULE34: "https://rule34.xxx/index.php?page=tags&s=list&tags=",
+  REALBOORU: "https://realbooru.com/index.php?page=tags&s=list&tags=",
+  SAFEBOORU: "https://safebooru.org/index.php?page=tags&s=list&tags=",
+  TBIB: "https://tbib.org/index.php?page=tags&s=list&tags=",
+  XBOORU: "https://xbooru.com/index.php?page=tags&s=list&tags=",
+  YANDERE: "https://yande.re/tag?name=",
+};
