4da346fd5bb1350f60730fc3017ddf97df3a899c
sinkaroid <bibsindra@gmail.com>
2022-08-20 06:02:52 +0700

    feat: reduce request boilerplate

--- src/modifier.ts
+++ src/modifier.ts
@@ -1,3 +1,26 @@
+import p from "phin";
+
+/**
+ * Scrape request
+ * @param {string} url - The url to scrape
+ * @param {string} query - The query to scrape
+ * @param {string} sort - The sorting method to use
+ * @param {string} useragent - Custom user agent
+ * @param {boolean} redirect - Apply redirects
+ * @return {Promise<p.IResponse>} Phin response
+ */
+export async function request(url: string, query: string, sort: string,
+  useragent: string, redirect: boolean): Promise<p.IResponse> {
+  const res = await p({
+    url: url + query + sort,
+    "headers": {
+      "User-Agent": useragent
+    },
+    followRedirects: redirect
+  });
+  return res;
+}
+
 export function htmlDecode(abc: string) {
   return abc
     .replace(/%28/g, "(")
