3675237e637938197fe187fac3371bb7228b6e8d
sinkaroid <bibsindra@gmail.com>
2022-08-20 06:03:31 +0700

    feat: add optional parseString method

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+// Client
 import Eiyuu from "./class/Eiyuu";
 
-export { Eiyuu };
+/**
+ * Optional methods
+ * Converts arrays with each string separated by a comma.
+ * @param {string[]} data - The array of strings to convert.
+ * @return {string} The converted string.
+ */
+function parseString(data: string[]): string {
+  return data.join(", ");
+}
+
+export { Eiyuu, parseString };
